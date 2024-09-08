5066648ee8870b484f563f3e0fbac9e8855fd881
subhankar <sub.nitt@gmail.com>
2024-09-08 16:45:39 +0530

    added files

--- generate_report.sh
+++ generate_report.sh
@@ -2,5 +2,14 @@
 rm -rf report.json
 cargo install markdown-test-report
 cargo test -- -Z unstable-options --report-time --format json >> report.json
+git config --global user.name "subhankar"
+git config --global user.emanil "biswas"
+mkdir contract-testing
+cd contract-testing
+git init
+git remote add origin https://github.com/subhankar-nitt/rust-contract-testing.git
+git pull
+git checkout test_id
 
-markdown-test-report -o index.md report.json
+cd ..
+markdown-test-report -o contract-testing/index.md report.json
