9b6e1346c66c31617e5b96d3fe946d1d7c2617a4
subhankar <sub.nitt@gmail.com>
2024-09-08 15:27:45 +0530

    added cloudbuild

--- generate_report.sh
+++ generate_report.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 rm -rf report.json
+cargo install markdown-test-report
 cargo test -- -Z unstable-options --report-time --format json >> report.json
 
 markdown-test-report -o index.md report.json
