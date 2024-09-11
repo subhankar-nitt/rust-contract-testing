793950531aad178f08266662f90d9f7b300abad5
root <root@localhost>
2024-09-11 06:27:46 +0000

    Added new files

--- tests/test_runner.rs
+++ tests/test_runner.rs
@@ -5,15 +5,14 @@ mod cloud_storage;
 mod idtoken;
 
 #[tokio::test]
-async fn user_test() -> Result<(), Box<dyn std::error::Error>> {
-    let consumer_name: &str = "consumer-1";
-    let producer_name: &str = "producer-2";
-    let provider_url: &str = "https://rust-server-986655996669.us-central1.run.app";
-    let test_name: &str = "test-user";
+async fn user_test() -> Result<(),Box<dyn std::error::Error>>{
 
-    user_test::push_data(
-        vec![
-            r#"
+    let consumer_name:&str = "consumer-1";
+    let producer_name:&str = "producer-2";
+    let provider_url:&str ="https://rust-server-986655996669.us-central1.run.app";
+    let test_name:&str = "test-user";
+
+    user_test::push_data( vec![r#"
     {
     "provider_state":"GET User With ID 1",
     "request":{
@@ -26,7 +25,8 @@ async fn user_test() -> Result<(), Box<dyn std::error::Error>> {
         "body":{"id":1,"user_name":"subhankar","comment":"user added "}
      }
      }"#,
-            r#"{
+
+    r#"{
         "provider_state":"GET User With ID 2",
         "request":{
             "method":"Get",
@@ -38,7 +38,8 @@ async fn user_test() -> Result<(), Box<dyn std::error::Error>> {
             "body":{"id":2,"user_name":"biswas","comment":"user added "}
          }
          }"#,
-            r#"{
+
+    r#"{
         "provider_state":"GET User With ID 3",
         "request":{
             "method":"Get",
@@ -49,22 +50,21 @@ async fn user_test() -> Result<(), Box<dyn std::error::Error>> {
             "headers":{"Content-Type":"application/json"},
             "body":{"id":3,"user_name":"ram","comment":"user added "}
          }
-         }"#,
-        ],
-        consumer_name,
-        producer_name,
-        provider_url,
-        test_name,
-    )
-    .await
-    .expect("calling init function ");
+         }"#
+
+
+    ],consumer_name,producer_name,provider_url,test_name)
+        .await.expect("calling init function ");
 
     user_test::contract_consumer().await.expect("Consumer failed");
-    let err = cloud_storage::uploadFile(format!("pacts/{}_test/{}-{}.json", test_name, consumer_name, producer_name)).await;
-    if err.is_err() {
-        println!("Error Saving file :{:?}", err);
+    let err = cloud_storage::uploadFile(format!("pacts/{}_test/{}-{}.json",test_name,consumer_name,producer_name)).await;
+    if err.is_err(){
+
+        println!("Error Saving file :{:?}",err);
     }
     user_test::contract_provider().await.expect("Provider failed");
 
+
     Ok(())
 }
+
