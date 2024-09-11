56f7e215de846f3ce355818f5f828717c79136e6
subhankar <sub.nitt@gmail.com>
2024-09-11 05:26:17 +0000

    Added new files

--- tests/test_runner.rs
+++ tests/test_runner.rs
@@ -1,5 +1,6 @@
 use std::fmt::format;
 
+
 mod user_test;
 mod cloud_storage;
 mod idtoken;
@@ -7,14 +8,14 @@ mod idtoken;
 #[tokio::test]
 async fn user_test() -> Result<(),Box<dyn std::error::Error>>{
 
-    let consumer_name:&str = "consumer";
-    let producer_name:&str = "producer";
+    let consumer_name:&str = "consumer-1";
+    let producer_name:&str = "producer-2";
     let provider_url:&str ="https://rust-server-986655996669.us-central1.run.app";
-    let test_name:&str = "user";
+    let test_name:&str = "test-user";
 
     user_test::push_data( vec![r#"
     {
-    "provider_state":"Get User With ID 1",
+    "provider_state":"GET User With ID 1",
     "request":{
         "method":"Get",
         "path":"/users/1"
@@ -27,18 +28,28 @@ async fn user_test() -> Result<(),Box<dyn std::error::Error>>{
      }"#,
 
     r#"{
-        "provider_state":"Get User With ID 2",
+        "provider_state":"GET User With ID 2",
         "request":{
             "method":"Get",
-            "path":"/users/2",
-            "headers": {
-                "Authorization": ""
-            }
+            "path":"/users/2"
         },
          "response":
             {"statusCode":200,
             "headers":{"Content-Type":"application/json"},
             "body":{"id":2,"user_name":"biswas","comment":"user added "}
+         }
+         }"#,
+
+        r#"{
+        "provider_state":"GET User With ID 3",
+        "request":{
+            "method":"Get",
+            "path":"/users/3"
+        },
+         "response":
+            {"statusCode":200,
+            "headers":{"Content-Type":"application/json"},
+            "body":{"id":3,"user_name":"ram","comment":"user added "}
          }
          }"#
 
